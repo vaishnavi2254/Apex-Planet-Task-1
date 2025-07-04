0585b690b6dc0b726dcecae7468a42898b5e45a9
vaishnavi2254 <127581164+vaishnavi2254@users.noreply.github.com>
2025-07-04 20:44:06 +0530

    Create woodpecker.js

---
+++ woodpecker.js
@@ -0,0 +1,20 @@
+// onloading event 
+window.onload = function () {
+    alert("Welcome to WoodPecker - Your Furniture Destination!");
+};
+// onclick alert on signup  
+function signup() {
+    if (confirm("Are you sure you want to SignUp?")) {
+        alert("Successfully Signed Up!");
+    } else {
+        alert("You clicked Cancel!");
+    }
+}
+// onclick alert on shop now
+function buyProduct() {
+    alert("Product added to your cart!");
+}
+// onclick alert on subscribe 
+function subscribe(){
+   alert("Yeah, You have Subscribed!");
+}
